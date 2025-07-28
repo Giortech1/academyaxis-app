dc74090628e526ba732cc47040db773c1b731995
giotechadmin <giotech_admin@giortech.com>
2025-07-28 09:45:10 +0100

    deleted some file from json

--- src/modules/student/index.js
+++
@@ -1,7 +0,0 @@
-// src/modules/student/index.js
-export { default as Dashboard } from './components/Dashboard';
-export { default as StudentProfile } from './components/StudentProfile';
-export { default as MyCourses } from './components/MyCourses';
-export { default as Grades } from './components/Grades';
-export { default as Assignments } from './components/Assignments';
-// ... export all student components
